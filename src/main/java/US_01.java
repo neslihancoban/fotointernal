988f5d044a189bfcadaf7790dbcfe0ba2fc11b42
Hilal Demir <hlldmr23@gmail.com>
2022-01-22 19:47:14 +0100

    new version

--- src/main/java/US_01.java
+++ src/main/java/US_01.java
@@ -2,5 +2,6 @@ public class US_01 {
     public static void main(String[] args) {
         System.out.println("we are working");
         System.out.println("second initial");
+        System.out.println("third initial");
     }
 }
